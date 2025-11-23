7d5944ece41ec1feb72d655a670b326df96853be
NexoWatt <info@nexowatt.com>
2025-11-23 22:28:36 +0100

    commit

--- main.js
+++ main.js
@@ -50,7 +50,6 @@ class NexoWattVis extends utils.Adapter {
     this.on('ready', this.onReady.bind(this));
     this.on('stateChange', this.onStateChange.bind(this));
     this.on('unload', this.onUnload.bind(this));
-    this.on('message', this.onMessage.bind(this));
   }
 
   
